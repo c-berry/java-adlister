2cbec5122caed9952b56b5b3450583c3733db680
Christopher Berry <christopher.m.berry01@gmail.com>
2022-04-25 16:47:20 -0500

    re-do

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,8 +6,32 @@ import java.io.IOException;
 
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
+
+    private int count = 1;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.getWriter().println("Hello World");
+
+        count += 1;
+        String name
+                = req.getParameter("name");
+        if (name == null){
+
+            name = "World";
+        }
+        if (req.getParameter("reset") != null){
+            count = 0;
+            res.sendRedirect("/hello");
+        }
+
+        res.getWriter().println("Hello, " + name);
+        res.getWriter().println("View count: " + count);
     }
+
+//    public int counter(int num){
+//        return num + 1;
+//    }
+
 }
+
+
