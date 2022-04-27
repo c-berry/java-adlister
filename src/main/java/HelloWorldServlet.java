bd8093ffaa9051cb91387f35ab310646a062cde0
Christopher Berry <christopher.m.berry01@gmail.com>
2022-04-27 11:47:33 -0500

    work through mvc-lecture

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,12 +1,24 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("<h1>Hello, World!</h1>");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String name = req.getParameter("name");
+        req.setAttribute("name", name);
+        RequestDispatcher view = req.getRequestDispatcher("/hello.jsp");
+        view.forward(req, resp);
     }
 }
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<head
+><title></title>
+</head>
+
+<body>
+
+<h1>Hi there, what's your name?</h1>
+<form method="post" action="Hello">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+    <button type="submit">Submit</button>
+</form>
+
+<c:if test = "${name != null}">
+    <p>Hello there, ${name}</p>
+</c:if>
+
+</body>
+</html>
